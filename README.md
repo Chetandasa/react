801efa1a597d3df28669fb3824828b356f09307f
Chetandasa <119198219+Chetandasa@users.noreply.github.com>
2023-02-17 16:40:13 +0530

    React.js Startup  and how to set the folders and

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react
 reactjs
+for react.js all folders in terinal enter this text to download 
+write in terinmal {npx create-react-app my-app} in place of my-app you can put your folder name link me react_startup 
---
+++ React_startup/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          hello
+        </p>
+        <h1>hello</h1>
+        <h2>hello world</h2>
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
